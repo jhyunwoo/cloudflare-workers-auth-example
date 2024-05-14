24e775237cd8fa2c03e3b47c96e25397ea063c30
jhyunwoo <jhyunwoo0228@gmail.com>
2024-05-14 23:48:05 +0900

    add auth

--- lib/cors.ts
+++ lib/cors.ts
@@ -1,5 +1,5 @@
 const corsHeaders = {
-	'Access-Control-Allow-Origin': 'http://localhost:3000',
+	'Access-Control-Allow-Origin': 'https://yonorder.moveto.kr',
 	'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
 	'Access-Control-Max-Age': '86400',
 	'Access-Control-Allow-Credentials': 'true',
